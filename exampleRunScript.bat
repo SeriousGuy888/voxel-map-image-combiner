a777cb1058e0b68b126c5bec2606f8b79067737a
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-12-26 23:34:38 -0500

    run.bat

--- exampleRunScript.bat
+++
@@ -1,3 +0,0 @@
-npm install
-node main.js C:\Users\Keris\Twitch\Minecraft\Instances\fabricWithVoxelmap\mods\mamiyaotaru\voxelmap\cache\test\overworld\images\z1
-PAUSE
---
+++ run.bat
@@ -0,0 +1,3 @@
+npm install
+node main.js D:\Games\.minecraft\voxelmap\cache\cheezsurv3.minehut.gg\overworld\overworld\images\z1
+pause
